ab96be6dc6034cd7eccc54e9c00ffdd984c7e99d
André Fernandes <estagiario.trinity@hotmail.com>
2022-10-11 16:11:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <iframe width="612" height="571" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%21804&authkey=%21ACEDawntfCLKfIU&em=2&wdAllowInteractivity=False&Item='Filipe%20Agencia'!B2%3AI26&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+    </div>
+  )
+}
+
+export default App
