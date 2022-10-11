4b2d37de7d8bfa8222f79a7eb010d8d1b9ec244a
André Fernandes <estagiario.trinity@hotmail.com>
2022-10-11 16:16:28 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <iframe width="612" height="571" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%21804&authkey=%21ACEDawntfCLKfIU&em=2&wdAllowInteractivity=False&Item='Filipe%20Agencia'!B2%3AI26&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+      <iframe width="612" height="571" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%21804&authkey=%21ACEDawntfCLKfIU&em=2&wdAllowInteractivity=False&Item='Filipe%20Agencia'!B2%3AI26&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
     </div>
   )
 }
