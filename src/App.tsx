dcd67875f72e42e4362d2b43abeddb1d5d425cc5
André Fernandes <estagiario.trinity@hotmail.com>
2022-10-17 16:03:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import "./styles/App.sass"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <iframe width="612" height="571" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%21804&authkey=%21ACEDawntfCLKfIU&em=2&wdAllowInteractivity=False&Item='Filipe%20Agencia'!B2%3AI26&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+      <iframe width="415" height="696" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%21804&authkey=%21ACEDawntfCLKfIU&em=2&wdAllowInteractivity=False&ActiveCell='Filipe%20Agencia'!B12&Item='Filipe%20Agencia'!A2%3AE28&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
     </div>
   )
 }
